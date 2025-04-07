531c7f175738ce3b0baaf2772d596ec198a4e555
fusionAMAN <786rockaman@gmail.com>
2025-04-07 11:32:32 +0530

    change in undirectedgraphcycle

--- undirectedgraphcycle.java
+++ undirectedgraphcycle.java
@@ -1,3 +1,42 @@
-public class undirectedgraphcycle {
-    
+
+
+class Solution {
+    public boolean isCycle(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Undirected: add both directions
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, -1, visited, adj)) {
+                    return true; // Cycle found
+                }
+            }
+        }
+
+        return false; // No cycle
+    }
+
+    private boolean dfs(int node, int parent, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
+        visited[node] = true;
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, node, visited, adj)) {
+                    return true;
+                }
+            } else if (neighbor != parent) {
+                return true; // Found a back-edge that isn't to the parent
+            }
+        }
+        return false;
+    }
 }
