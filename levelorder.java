58b2e5173c5e591b0293871be4b3458fca21d4c7
fusionAMAN <786rockaman@gmail.com>
2025-04-08 18:39:48 +0530

    day 7

---
+++ levelorder.java
@@ -0,0 +1,153 @@
+/**
+ * 
+ Level Order Traversal of a binary tree means visiting each node level by level from top to bottom and from left to right at each level.
+This is typically done using a Queue data structure (Breadth First Search).
+
+ */
+
+ //{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            ArrayList<ArrayList<Integer>> res = g.levelOrder(root);
+            for (ArrayList<Integer> level : res)
+                for (Integer num : level) System.out.print(num + " ");
+            System.out.println();
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
+        // Your code here
+       ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+       Queue<Node>q=new LinkedList<>();
+       q.add(root);
+       while(!q.isEmpty()){
+        int size=q.size();
+        ArrayList<Integer>currlevel=new ArrayList<>();
+        for(int i=0;i<size;i++){
+            Node current=q.poll();
+            currlevel.add(current.data);
+
+            if(current.left!=null){
+                q.add(current.left);
+            }
+            if(current.right!=null){
+                q.add(current.right);
+            }
+        }
+        res.add(currlevel);
+       }
+       return res;
+    }
+}
--- undirectedgraphcycle.java
+++ undirectedgraphcycle.java
@@ -40,3 +40,57 @@ class Solution {
         return false;
     }
 }
+
+/* 
+ * class Solution {
+    public boolean isCycle(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Undirected: add both directions
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (bfs(i, visited, adj)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean bfs(int start, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start, -1}); // {current_node, parent}
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int node = curr[0];
+            int parent = curr[1];
+
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(new int[]{neighbor, node});
+                } else if (neighbor != parent) {
+                    // Visited but not parent => cycle
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+*/
