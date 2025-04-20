8e1359aa9cab77068eea7dbec71c62a5cc502b2e
fusionAMAN <786rockaman@gmail.com>
2025-04-21 00:44:08 +0530

    checkforBST

--- checkforBST.java
+++ checkforBST.java
@@ -1,12 +1,18 @@
-class Solution{
-    boolean isBST(Node root){
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to check whether a Binary Tree is BST or not.
+    boolean isBST(Node root) {
+        // code here
         return check(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
     }
-    boolean check(Node root, int min, int max){
+    public Boolean check(Node root, int min,int max){
         if(root==null)return true;
         if(root.data<min || root.data>max){
             return false;
         }
-        return check(root.left,min,root.data)&& check(root.right,root.data,max);
+        return check(root.left,min,root.data) && check(root.right,root.data,max);
     }
 }
