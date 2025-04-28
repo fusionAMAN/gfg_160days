db579ba7d263768df60e5e30fe1f8fa8ac3ca790
fusionAMAN <786rockaman@gmail.com>
2025-04-28 14:14:43 +0530

    Deque

---
+++ ksizedsubarraymax.java
@@ -0,0 +1,29 @@
+public class ksizedsubarraymax {
+   
+    static ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            // Remove indexes of elements not in the current window
+            if (!dq.isEmpty() && dq.peekFirst() == i - k) {
+                dq.pollFirst();
+            }
+            
+            // Remove elements smaller than the current element from the back
+            while (!dq.isEmpty() && arr[i] >= arr[dq.peekLast()) {
+                dq.pollLast();
+            }
+            
+            // Add current element's index at the back
+            dq.offerLast(i);
+            
+            // When we have a valid window
+            if (i >= k - 1) {
+                result.add(arr[dq.peekFirst()]);
+            }
+        }
+        
+        return result;
+    }
+}
