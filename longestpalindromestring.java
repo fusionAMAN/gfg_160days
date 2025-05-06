c79e1224557809bb9003d4b2143910205fb8a112
fusionAMAN <786rockaman@gmail.com>
2025-05-06 15:44:42 +0530

    dp

---
+++ longestpalindromestring.java
@@ -0,0 +1,40 @@
+public class longestpalindromestring {
+    static String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 0) return "";
+
+        boolean[][] dp = new boolean[n][n];
+        int start = 0;
+        int maxLength = 1;
+
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++)
+            dp[i][i] = true;
+
+        // Check for substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        // Check for substrings of length > 2
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    if (len > maxLength) {
+                        start = i;
+                        maxLength = len;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+}
